d3bfe36ddc6b6a6e8574a426ea36fb9be2c8e4ef
amakaroff82 <amakaroff82@gmail.com>
2018-06-06 23:32:51 +0300

    small fixes

--- server/express-server.ts
+++ server/express-server.ts
@@ -1,11 +1,9 @@
 import { Facenet } from '../'
 import { IncomingMessage, ServerResponse, Server, createServer } from 'http';
-import { extname } from 'path';
-import { readFile, writeFile, unlink } from 'fs';
-//import {saveImage} from '../src/misc';
+import { writeFile, unlink } from 'fs';
+import { serveStatic } from './static-server';
 
 const PORT = 8080;
-const STATIC_FILES_DIR = 'admin';
 
 export class FNServer {
   private facenet: Facenet
@@ -32,70 +30,6 @@ export class FNServer {
       this.isReady = true;
       console.log('Facenet started');
     }
-  }
-
-  private serveStatic(request: IncomingMessage, response: ServerResponse) : void {
-
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    response.setHeader('Access-Control-Allow-Credentials', 'true');
-
-
-    let filePath = request.url;
-
-    if (filePath == '/')
-      filePath = '/index.html';
-
-    filePath = __dirname + '/' + STATIC_FILES_DIR + filePath;
-
-    const ext = extname(filePath);
-    let contentType = 'text/html';
-    switch (ext) {
-      case '.js':
-        contentType = 'text/javascript';
-        break;
-      case '.css':
-        contentType = 'text/css';
-        break;
-      case '.json':
-        contentType = 'application/json';
-        break;
-      case '.png':
-        contentType = 'image/png';
-        break;
-      case '.jpg':
-        contentType = 'image/jpg';
-        break;
-      case '.wav':
-        contentType = 'audio/wav';
-        break;
-    }
-
-    readFile(filePath, (error, content) => {
-
-      if (error) {
-        if(error.code == 'ENOENT'){
-          readFile('./404.html', function(error, content) {
-            if(error) {
-              response.writeHead(500);
-              response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            } else {
-              response.writeHead(200, { 'Content-Type': contentType });
-              response.end(content, 'utf-8');
-            }
-          });
-        }
-        else {
-          response.writeHead(500);
-          response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-        }
-      }
-      else {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
-      }
-    });
   }
 
   private serveApi(request: IncomingMessage, response: ServerResponse) : void {
@@ -156,10 +90,9 @@ export class FNServer {
     this.server = createServer((request, response) => {
 
         if(request.method === "POST" && request.url && request.url.indexOf('/api') === 0){
-          console.log(">>> POST");
           this.serveApi(request, response);
         } else if (request.method === "GET"){
-          this.serveStatic(request, response);
+          serveStatic(request, response);
         } else {
           response.end();
         }
---
+++ server/static-server.ts
@@ -0,0 +1,67 @@
+import {IncomingMessage, ServerResponse} from 'http';
+import {readFile} from 'fs';
+import {extname} from 'path';
+
+const STATIC_FILES_DIR = 'admin';
+
+export function serveStatic(request: IncomingMessage, response: ServerResponse) : void {
+  response.setHeader('Access-Control-Allow-Origin', '192.168.1.77');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  let filePath = request.url;
+
+  if (filePath === '/')
+    filePath = '/index.html';
+
+  filePath = __dirname + '/' + STATIC_FILES_DIR + filePath;
+
+  const ext = extname(filePath);
+  let contentType = 'text/html';
+  switch (ext) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.wav':
+      contentType = 'audio/wav';
+      break;
+  }
+
+  readFile(filePath, (error, content) => {
+
+    if (error) {
+      if(error.code === 'ENOENT'){
+        readFile('./404.html', function(error, content) {
+          if(error) {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+          } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+          }
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+      }
+    }
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
+}
